e89c45ff9726f4cd0b8136f45f8e96fc2851afb3
Julian Stieß <j_stiess17@cs.uni-kl.de>
2021-01-14 17:07:24 +0100

    Last access shown on Clientview

--- led_colors/forms.py
+++ led_colors/forms.py
@@ -1,9 +1,15 @@
-from django.forms import ModelForm, ValidationError, ModelChoiceField, Select, Form
+from django.forms import ModelForm, ValidationError, ModelChoiceField, Select, Form, CharField
 from .models import Client, ColorProfile
 class ClientForm(ModelForm):
     class Meta:
         model = Client
         fields = ['name',"profile_choice"]
+    def __init__(self, *args, **kwargs):
+        # only change attributes if an instance is passed            
+        instance = kwargs.get('instance')
+        if instance:
+            self.base_fields['ip'].initial = instance.ip.decode("utf-8")
+        super().__init__(*args, **kwargs)
     profile_choice = ModelChoiceField(
         queryset = ColorProfile.objects.all(),
         empty_label=None,
@@ -12,6 +18,7 @@ class ClientForm(ModelForm):
         label='ColorProfile:',
         widget=Select(attrs={'class': 'colored'})
     )
+    ip = CharField(disabled=True,label="Last access IP")
 class MasterColor(Form):
     profile_choice = ModelChoiceField(
         queryset = ColorProfile.objects.all(),
