cf9acad7736d5db8689597f79e44cad8718da1c8
Julian Stieß <j_stiess17@cs.uni-kl.de>
2021-03-14 02:14:00 +0100

    fix: asgi changes so it actually works in fucking prod

--- home_state/asgi.py
+++ home_state/asgi.py
@@ -9,15 +9,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-import realtime.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_state.settings')
+app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import realtime.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             realtime.routing.websocket_urlpatterns
