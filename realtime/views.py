31cac27d2b1a00134acdc4ca1976a02cf3581b01
Julian Stieß <j_stiess17@cs.uni-kl.de>
2021-03-17 16:51:27 +0100

    fix: websockets now over wss if possible

--- realtime/views.py
+++ realtime/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from led_colors.models import Client, ColorProfile
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponseRedirect
 # Create your views here.
 def realtime(request, key):
     user = request.user
@@ -9,3 +9,4 @@ def realtime(request, key):
         master_color,_ = ColorProfile.objects.get_or_create(name="master_"+str(user.id))
         Client.manager.set_realtime(user.id,True)
         return render(request, 'realtimemaster.html',{'current_color':master_color,'key': key})
+    return HttpResponseRedirect(reverse("login")) 
